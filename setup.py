0296ac4d9cc66797e5d6c99da49dee06414ca6e0
Johan Frisell <johan.henric.frisell@gmail.com>
2015-09-17 20:57:13 +0200

    adds PyPI files

---
+++ setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+
+
+setup(
+    name = 'django-random-color-picker-field',
+    packages = ['django-random-color-picker-field'],
+    version = '0.0.1',
+    description = 'A color picker field for Django admin that lets you pick a\
+    color from 24 randomly generated colors.',
+    author = 'Johan Frisell',
+    author_email = 'johan@trell.se',
+    url = 'https://github.com/frisellcpl/django-random-color-picker-field',
+    download_url = 'https://github.com/frisellcpl/django-random-color-picker-fi\
+    eld/tarball/0.0.1',
+    keywords = ['django', 'colorpicker', 'django admin', 'color picker'],
+    classifiers = [],
+)
