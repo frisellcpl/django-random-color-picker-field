55f78f1d05e148c42a5115ec87e5411dc524047a
Johan Frisell <johan.henric.frisell@gmail.com>
2015-09-18 13:51:47 +0200

    removes breaklines from setup.py

--- setup.py
+++ setup.py
@@ -5,13 +5,11 @@ setup(
     name = 'django-random-color-picker-field',
     packages = ['django-random-color-picker-field'],
     version = '0.0.1',
-    description = 'A color picker field for Django admin that lets you pick a\
-    color from 24 randomly generated colors.',
+    description = 'A color picker field for Django admin that lets you pick a color from 24 randomly generated colors.',
     author = 'Johan Frisell',
     author_email = 'johan@trell.se',
     url = 'https://github.com/frisellcpl/django-random-color-picker-field',
-    download_url = 'https://github.com/frisellcpl/django-random-color-picker-fi\
-    eld/tarball/0.0.1',
+    download_url = 'https://github.com/frisellcpl/django-random-color-picker-field/tarball/0.0.1',
     keywords = ['django', 'colorpicker', 'django admin', 'color picker'],
     classifiers = [],
 )
