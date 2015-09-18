6ca0f355a918caceff80b4ac210cf3951cc85044
Johan Frisell <johan.henric.frisell@gmail.com>
2015-09-18 13:59:22 +0200

    bumped version

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ from distutils.core import setup
 setup(
     name = 'django-random-color-picker-field',
     packages = ['django-random-color-picker-field'],
-    version = '0.0.1',
+    version = '1.0.0',
     description = 'A color picker field for Django admin that lets you pick a color from 24 randomly generated colors.',
     author = 'Johan Frisell',
     author_email = 'johan@trell.se',
     url = 'https://github.com/frisellcpl/django-random-color-picker-field',
-    download_url = 'https://github.com/frisellcpl/django-random-color-picker-field/tarball/0.0.1',
+    download_url = 'https://github.com/frisellcpl/django-random-color-picker-field/tarball/1.0.0',
     keywords = ['django', 'colorpicker', 'django admin', 'color picker'],
     classifiers = [],
 )
