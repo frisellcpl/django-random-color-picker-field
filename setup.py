98775d3f63d637cef0fe72c4f823486dec9458d1
Johan Frisell <johan.henric.frisell@gmail.com>
2015-09-19 08:49:11 +0200

    updates manifest and pypi metadata

--- setup.py
+++ setup.py
@@ -3,13 +3,38 @@ from distutils.core import setup
 
 setup(
     name = 'djangorcp',
-    packages = ['djangorcp'],
-    version = '1.0.3',
+    packages = [
+        'djangorcp'
+    ],
+    version = '1.0.4',
     description = 'A color picker field for Django admin that lets you pick a color from 24 randomly generated colors.',
     author = 'Johan Frisell',
     author_email = 'johan@trell.se',
+    requires = [
+        'Django (>=1.7)',
+    ],
     url = 'https://github.com/frisellcpl/django-random-color-picker-field',
-    download_url = 'https://github.com/frisellcpl/django-random-color-picker-field/tarball/1.0.3',
-    keywords = ['django', 'colorpicker', 'django admin', 'color picker'],
-    classifiers = [],
+    download_url = 'https://github.com/frisellcpl/django-random-color-picker-field/tarball/1.0.4',
+    keywords = [
+        'django',
+        'colorpicker',
+        'django admin',
+        'color picker'
+    ],
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+    ]
 )
